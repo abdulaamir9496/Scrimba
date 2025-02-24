43ded689fffcce88d88e5cdc6c1840a5075e4146
Abdul Aamir <abdulaamir9496@gmail.com>
2025-02-24 14:05:57 +0530

    Build a BlackJack project using (arrays, objects, booleans, if else statements, comparison operators, logical operators, for loops, the Math object, and return statements).

--- Build a Blackjack Game/index1.js
+++ Build a Blackjack Game/index1.js
@@ -144,26 +144,84 @@
 
 //Link to stylesheet
 
-let firstCard = 10
-let secondCard = 4
-// 1. Create a new array - cards - that contains firstCard and secondCard
-let cards = [firstCard, secondCard];  //array of cards - ordered list of items
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+// 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {
+    name: "Per",
+    chips: 200,
+    sayHello: function() {
+        console.log("Heisann!")
+    }
+}
 
+player.sayHello();  //player is the object, sayHello() is the function of that object which is also called as method of that object.
+//For example we have same way:
+// console.log();
+// Math.random();
+// Math.floor();
+// etc.,
+
+
+// 2. Use getRandomCard() to set the values of firstCard and secondCard
+
+// let firstCard = getRandomCard();
+// let secondCard = getRandomCard();
+// 1. Create a new array - cards - that contains firstCard and secondCard
+// let cards = [firstCard, secondCard];  //array of cards - ordered list of items
+let cards = [];
+// let sum = firstCard + secondCard
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
 // 1. Store the message-el paragraph in a variable called messageEl
 let messageEl = document.getElementById("message-el");
 // 2. Store the sum paragraph in a variable called sumEl
 let sumEl = document.getElementById("sum-el");
 // let sumEl = document.querySelector("#sum-el");   //Another way of selecting the element using querySelector. It selects the first element that matches the selector. It can be used to select elements by class, id, tag name, etc. It can also be used to select multiple elements by class, id, tag name and other selectors.
 
-// 2. Store the cards paragraph in a variable called cardsEl
+// 3. Store the cards paragraph in a variable called cardsEl
 let cardsEl = document.getElementById("cards-el");
+
+// let playerName = "Per";
+// let playerChips = 145;
+
+// let playerEl = document.getElementById("player-el");
+// playerEl.textContent = playerName + ": $" + playerChips;
+
+// console.log(cards);
+
+// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
+    let playerEl = document.getElementById("player-el");
+
+// 4. Render the player's name and chips in playerEl
+    playerEl.textContent = player.name + ": $" + player.chips;
+
+// 1. Create a function, getRandomCard(), that always returns the number 5
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+    // return 5;
+    // if 1     -> return 11
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if(randomNumber > 10) {
+        return 10;
+    }
+    else if(randomNumber === 1) {
+        return 11;
+    }
+    else {
+        return randomNumber;
+    }
+}
 
 // Create a new function called startGame() that calls renderGame()
 function startGame() {
+    isAlive = true;
+    // Generate two random numbers
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    // Re-assign the cards and sum variables so that the game can start
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame();
 }
 
@@ -171,7 +229,12 @@ function renderGame() {
     // 2. Refer to the cards array when rendering out the cards
     //render out firstCard and secondCad
     // 3. Render the cars on the page using this format -> "Cards: 10 4"
-    cardsEl.textContent  = "Cards: " + cards[0] + " " + cards[1];
+    cardsEl.textContent  = "Cards: ";
+    // Create a for loop that renders out all the cards instead of just two
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+
     //render out ALL the cards we have
     // 3. Render the sum on the page using this format -> "Sum: 14"
     sumEl.textContent = "Sum: " + sum;
@@ -193,15 +256,21 @@ function renderGame() {
 
 // 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
 function newCard() {
-    // console.log("Drawing a new card from the deck!");
-    // 1. Create a card variable, and hard code its value to a number (2-11)
-    let card = 8;
-    // 2. Add the new card to the sum variable
-    sum += card;
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if(isAlive === true && hasBlackJack === false) {
 
-    // Push the card to the cards array
-    cards.push(card);
-    console.log(cards);
-    // 3. Call startGame()
-    renderGame();
+        // 3. Use the getRandomCard() to set the value of card
+
+        // console.log("Drawing a new card from the deck!");
+        // 1. Create a card variable, and hard code its value to a number (2-11)
+        let card = getRandomCard();
+        // 2. Add the new card to the sum variable
+        sum += card;
+
+        // Push the card to the cards array
+        cards.push(card);
+        // console.log(cards);
+        // 3. Call startGame()
+        renderGame();
+    }
 }
